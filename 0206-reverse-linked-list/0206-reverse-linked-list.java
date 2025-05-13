799affe88d9fa3d68d42eb6d41f81f5f785f2996
swarnenduduttas <swarnenduduttas@gmail.com>
2025-05-13 22:46:13 +0530

    Time: 0 ms (100%), Space: 42.5 MB (66.27%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,7 +10,8 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null)
+        //ITERATIVE
+        /** if (head == null || head.next == null)
             return head;
         ListNode prev = null;
         ListNode temp = head;
@@ -21,6 +22,15 @@ class Solution {
             prev = temp;
             temp = front;
         }
-        return prev;
+        return prev; **/
+
+        //RECURSIVE
+        if(head == null || head.next == null)
+            return head;
+        ListNode newHead = reverseList(head.next);
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+        return newHead;
     }
 }
