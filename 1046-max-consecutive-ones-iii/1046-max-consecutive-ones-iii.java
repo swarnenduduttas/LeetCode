dcadc2cb9ee214701ff70c0a24f5c8f5ed049416
swarnenduduttas <swarnenduduttas@gmail.com>
2025-05-29 00:22:04 +0530

    Time: 3 ms (91.92%), Space: 46.5 MB (69.04%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.java
@@ -6,21 +6,21 @@ class Solution {
         int r = 0;
         int zeroes = 0;
         while (r < nums.length) {
-            if(nums[r] == 0)
+            if (nums[r] == 0) {
                 zeroes++;
-            
-            while(zeroes > k){
-               if(nums[l] == 0)
+            }
+            if (zeroes > k) {
+                if(nums[l] == 0)
                     zeroes--;
                 l++;
             }
-            
-             //if(zeroes<=k) {
-                len = r - l + 1;
+            if(zeroes <= k){
+                len = r-l+1;
                 maxLen = Math.max(maxLen, len);
-           // }
+            }
             r++;
         }
+
         return maxLen;
     }
 }
