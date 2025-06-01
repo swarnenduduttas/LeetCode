75fb287add5a491fa02e9df18cb3297e5c83a5db
swarnenduduttas <swarnenduduttas@gmail.com>
2025-06-01 22:13:19 +0530

    Time: 8 ms (77.22%), Space: 48.5 MB (27.1%) - LeetHub

--- 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/1408-find-the-smallest-divisor-given-a-threshold.java
@@ -1,34 +1,27 @@
 class Solution {
-    public int smallestDivisor(int[] arr, int limit) {
-        int n = arr.length; //size of array.
-        if(n > limit) return -1;
-        //Find the maximum element:
-        int maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            maxi = Math.max(maxi, arr[i]);
-        }
-
-        int low = 1, high = maxi;
-
-        //Apply binary search:
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (sumByD(arr, mid) <= limit) {
-                high = mid - 1;
+    public int smallestDivisor(int[] nums, int threshold) {
+        int low = 1;
+        int high = Arrays.stream(nums).max().getAsInt();
+        int ans = 0;
+        while(low<= high){
+            int mid = (low+high)/2;
+            if(possible(nums, threshold, mid)){
+                ans = mid;
+                high = mid-1;
             } else {
-                low = mid + 1;
+                low = mid+1;
             }
         }
-        return low;
+
+        return ans;
+
     }
 
-    public static int sumByD(int[] arr, int div) {
-        int n = arr.length; //size of array
-        //Find the summation of division values:
+    boolean possible(int[] nums, int threshold, int mid){
         int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += Math.ceil((double)(arr[i]) / (double)(div));
+        for(int i =0; i<nums.length; i++){
+            sum += Math.ceilDiv(nums[i], mid);
         }
-        return sum;
+        return sum<= threshold;
     }
 }
