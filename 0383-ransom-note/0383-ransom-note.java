c0901e0101033189c5285ded7d57540a35ba806e
swarnenduduttas <swarnenduduttas@gmail.com>
2026-02-17 21:26:12 +0530

    Time: 14 ms (39.01%), Space: 47.1 MB (37.08%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> ranMap = new HashMap<>();
-        for(Character c : ransomNote.toCharArray()){
-            ranMap.put(c, ranMap.getOrDefault(c,0)+1);
-        }
         HashMap<Character, Integer> magMap = new HashMap<>();
         for(Character c : magazine.toCharArray()){
             magMap.put(c, magMap.getOrDefault(c,0)+1);
         }
-        for(Map.Entry<Character, Integer> e: ranMap.entrySet()){
-            if(!magMap.containsKey(e.getKey()) || magMap.get(e.getKey()) < e.getValue()){
-                return false;
-            }
+       for(Character c : ransomNote.toCharArray()){
+        if(!magMap.containsKey(c) || magMap.get(c) == 0){
+            return false;
         }
+        magMap.put(c, magMap.get(c)-1);
+       }
         return true;
     }
 }
