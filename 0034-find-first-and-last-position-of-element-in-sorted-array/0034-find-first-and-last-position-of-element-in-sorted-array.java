ed60c5102fa4f920266d26e29a991c6c26029594
swarnenduduttas <swarnenduduttas@gmail.com>
2026-03-06 00:03:14 +0530

    Time: 0 ms (100%), Space: 48.2 MB (70.83%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,45 +1,44 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        return new int[]{firstIndex(nums, target), lastIndex(nums, target)};
+        return new int[] { firstPos(nums, target), lastPos(nums, target) };
     }
 
-    private int lastIndex(int[] nums, int target) {
+    int firstPos(int[] nums, int target) {
         int low = 0;
-        int high = nums.length-1;
-        int mid;
-        int ans = -1;
+        int high = nums.length - 1;
+        int res = -1;
+        
         while (low <= high) {
-            mid = (low + high) / 2;
-
-            if(nums[mid] == target){
-                ans = mid;
-                low = mid+1;
-            } else if (target<nums[mid]) {
-                high = mid-1;
+            int mid = (high + low) / 2;
+            if (target < nums[mid]) {
+                high = mid - 1;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
             } else {
+                res = mid;
+                high = mid-1;
+            }
+        }
+        return res;
+    }
+
+    int lastPos(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int res = -1;
+
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            if (target < nums[mid]) {
+                high = mid - 1;
+            } else if (target > nums[mid]) {
+                low = mid + 1;
+            } else {
+                res = mid;
                 low = mid+1;
             }
         }
-        return ans;
+        return res;
     }
 
-    private int firstIndex(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length-1;
-        int mid;
-        int ans = -1;
-        while (low <= high) {
-            mid = (low + high) / 2;
-
-            if (nums[mid] == target) {
-                ans = mid;
-                high = mid-1;
-            } else if (target<nums[mid]) {
-                high = mid-1;
-            } else {
-                low = mid+1;
-            }
-        }
-        return ans;
-    }
 }
