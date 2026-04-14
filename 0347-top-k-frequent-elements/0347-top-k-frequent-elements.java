bd601147eccade41984cda0027500d0fa96902c7
swarnenduduttas <swarnenduduttas@gmail.com>
2026-04-14 18:33:13 +0530

    Time: 13 ms (80.09%), Space: 48 MB (14.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,25 +1,28 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map1 = new HashMap<>();
-
-        for(int i = 0; i < nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             map1.put(nums[i], map1.getOrDefault(nums[i],0)+1);
         }
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (a,b)->{
-                return b.second - a.second;
-            }
-        );
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.count - b.count);
 
-        for(Map.Entry<Integer, Integer> e : map1.entrySet()) {
-            pq.add(new Pair(e.getKey(), e.getValue()));
+        int count = 0;
+        for(Map.Entry<Integer, Integer> e : map1.entrySet()){
+            if(pq.size() != k){
+                pq.add(new Pair(e.getValue(),e.getKey()));
+                continue;
+            }
+            if(pq.peek().count < e.getValue()){
+                pq.poll();
+                pq.add(new Pair(e.getValue(),e.getKey()));
+            }
         }
 
         int[] result = new int[k];
 
-        for(int i = 0; i<k; i++){
-            result[i] = pq.poll().first;
+        for(int i =0; i<k; i++){
+            result[i] = pq.poll().member;
         }
 
         return result;
@@ -27,12 +30,12 @@ class Solution {
     }
 
     class Pair {
-        int first;
-        int second;
+        int count;
+        int member;
 
-        public Pair(int first, int second){
-            this.first = first;
-            this.second = second;
+        public Pair(int count, int member) {
+            this.count = count;
+            this.member = member;
         }
     }
 }
