60a02de4f0a02c72ed8ccaba86f54eee948697d5
swarnenduduttas <swarnenduduttas@gmail.com>
2026-07-12 20:38:32 +0530

    Time: 1 ms (99.83%), Space: 47.8 MB (57.21%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,27 +1,27 @@
 class Solution {
-   public void solve(char[][] board) {
+    public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
-                for(int j =0; j<m;j++){
-            if(board[0][j] == 'O'){
+        for (int j = 0; j < m; j++) {
+            if (board[0][j] == 'O') {
                 dfs(board, n, m, 0, j);
             }
-            if(board[n-1][j] == 'O'){
-                dfs(board, n, m, n-1, j);
+            if (board[n - 1][j] == 'O') {
+                dfs(board, n, m, n - 1, j);
             }
         }
-        
-        for(int i =0; i<n; i++){
-            if(board[i][0] == 'O'){
+
+        for (int i = 0; i < n; i++) {
+            if (board[i][0] == 'O') {
                 dfs(board, n, m, i, 0);
             }
-            if(board[i][m-1] == 'O'){
-                dfs(board, n, m, i, m-1);
+            if (board[i][m - 1] == 'O') {
+                dfs(board, n, m, i, m - 1);
             }
         }
         for (int p = 0; p < n; p++) {
             for (int q = 0; q < m; q++) {
-                if(board[p][q] == '#'){
+                if (board[p][q] == '#') {
                     board[p][q] = 'O';
                 } else {
                     board[p][q] = 'X';
@@ -33,12 +33,13 @@ class Solution {
     private boolean isValidPos(int n, int m, int row, int col) {
         if (row >= n || row < 0) {
             return false;
-        } else return col < m && col >= 0;
+        } else
+            return col < m && col >= 0;
     }
 
     private void dfs(char[][] board, int n, int m, int i, int j) {
-        int[] dX = {-1, 1, 0, 0};
-        int[] dY = {0, 0, 1, -1};
+        int[] dX = { -1, 1, 0, 0 };
+        int[] dY = { 0, 0, -1, 1 };
         board[i][j] = '#';
         for (int k = 0; k < 4; k++) {
             int row = i + dX[k];
